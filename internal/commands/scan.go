3affcc1a8ad5988efa8ed900a0449ca45697c741
chetanjangir0 <chetanrjj2002@gmail.com>
2025-09-23 19:06:53 +0530

    refactor : add scan.go

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -4,20 +4,20 @@ import (
 	"context"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/chetanjangir0/blueboy/internal/network"
 	"github.com/chetanjangir0/blueboy/internal/ui"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
-func ConnectDevice(UUID string, nm network.NetworkManager) tea.Cmd {
+
+func StartScan(nm network.NetworkManager) tea.Cmd {
 	return func() tea.Msg {
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
-
-		err := nm.Connect(ctx, UUID)
+		devices, err := nm.ScanDevices(ctx)
 		if err != nil {
-			return ui.ConnectDeviceMsg{Err: err}
+			return ui.StartScanMsg{Err: err}
 		}
-		return ui.ConnectDeviceMsg{Output: "connection successfully activated"}
+		return ui.StartScanMsg{Devices: devices}
 	}
+
 }
--- internal/network/manager.go
+++ internal/network/manager.go
@@ -2,16 +2,11 @@ package network
 
 import "context"
 
-type Device struct {
-	Name     string
-	Type     string
-	UUID     string
-	Security string
-}
+import "github.com/chetanjangir0/blueboy/internal/ui"
 
 type NetworkManager interface {
-	FetchPaired(ctx context.Context) ([]Device, error)
-	ScanDevices(ctx context.Context) ([]Device, error)
+	FetchPaired(ctx context.Context) ([]ui.Device, error)
+	ScanDevices(ctx context.Context) ([]ui.Device, error)
 	Connect(ctx context.Context, UUID string) error
-	Pair(ctx context.Context, device Device, password string) error
+	Pair(ctx context.Context, device ui.Device, password string) error
 }
