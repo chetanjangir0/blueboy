c6d46ca4c194b4346fdfec195fa0cf4be9f21943
chetanjangir0 <chetanrjj2002@gmail.com>
2025-09-23 19:21:18 +0530

    refactor: add paired.go

---
+++ internal/commands/paired.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"context"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/chetanjangir0/blueboy/internal/network"
+	"github.com/chetanjangir0/blueboy/internal/ui"
+)
+
+func fetchPaired(nm network.NetworkManager) tea.Cmd {
+	return func() tea.Msg {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		devices, err := nm.FetchPaired(ctx)
+		if err != nil {
+			return ui.StartScanMsg{Err: err}
+		}
+
+		return ui.FetchPairedMsg{Devices: devices}
+	}
+}
